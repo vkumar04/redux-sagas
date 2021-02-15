54d46d1ce4f7c7dbb6fd8c395ccdee66a66710f9
vick <vick.kumar44@gmail.com>
2021-02-14 20:28:52 -0500

    installed tailwind css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import App from './App';
