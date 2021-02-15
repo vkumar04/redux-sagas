03b34c34c52d966c042e3cd97d24b79b151159dc
vick <vick.kumar44@gmail.com>
2021-02-14 21:08:40 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   variants: {
